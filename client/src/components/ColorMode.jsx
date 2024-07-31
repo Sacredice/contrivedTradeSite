4936be7f62a4fc802862d497f8970d89a9993256
Sacredice <122034500+Sacredice@users.noreply.github.com>
2024-07-31 23:33:37 +0300

    chart dependency require import problem fixed in vite.config.js

--- client/src/components/ColorMode.jsx
+++ client/src/components/ColorMode.jsx
@@ -7,7 +7,7 @@ function ColorMode() {
     const { checked, setChecked } = useColorMode();
 
     return (
-        <Box sx={{ display: { xs: "none", sm: "flex"}, flexDirection: "row", justifyContent: "end", mx: 1, pt: 1}}>
+        <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "end", mx: 1, pt: 1}}>
             <Box sx={{  display: "flex", flexDirection: "row", paddingRight: "4px", paddingLeft: "0"}}>
                 <Typography sx={{ marginTop: "7px", color: checked ? "whitesmoke" : "black" }} >Dark Mode</Typography>
                 <Switch
--- client/src/config/firebaseConfig.js
+++ client/src/config/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "tradegame-6e7fb.firebaseapp.com",
-  projectId: "tradegame-6e7fb",
-  storageBucket: "tradegame-6e7fb.appspot.com",
-  messagingSenderId: "1016240506083",
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
-  measurementId: "G-SHXFRLSLG5"
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
