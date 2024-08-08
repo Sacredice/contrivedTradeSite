471bb95a9c21840131079b77c5451569abaddf3d
Sacredice <122034500+Sacredice@users.noreply.github.com>
2024-08-08 17:34:12 +0300

    static current price bug fixed

--- server/utilities/randomIntervals.js
+++ server/utilities/randomIntervals.js
@@ -46,6 +46,161 @@ function keepServerUp() {
     }, 14.5 * 60 * 1000)
 }
 
+
+const randomNewPrice = async (material) => {
+    // check if price reach any tempLimit true => set new temp limit
+    const matObj = (await db.collection('prices').doc(material).get()).data();
+    clearOldHistoryData(material, matObj);
+    const increase = randomNumber(matObj.limits.maxAmount);
+    // there is no price set before limit check. if price is already off the tempLimit
+    if (matObj.currentPrice < matObj.limits.tempMinLimit) {
+        const newPrice = matObj.currentPrice + Math.floor(increase / 2);
+        await updateNewPrice(material, newPrice);
+        return
+    } else if (matObj.currentPrice > matObj.limits.tempMaxLimit) {
+        const newPrice = matObj.currentPrice - Math.floor(increase / 2);
+        await updateNewPrice(material, newPrice);
+        return
+    }
+    // if the increase roll greater than half of the increase roll price increase, otherwise lesser than the half of the increase price decrease
+    const priceChange = increase - Math.floor(matObj.limits.maxAmount / 2);
+    const newPrice = matObj.balancer !== 0 
+        ? matObj.currentPrice + priceChange + matObj.balancer * matObj.limits.maxAmount / 10 
+        : matObj.currentPrice + priceChange;
+
+    if (newPrice < matObj.limits.tempMinLimit || newPrice > matObj.limits.tempMaxLimit) {
+        switch (material) {
+            case "gold":
+                const staticGoldLimit = newPrice < matObj.limits.tempMinLimit ? GOLD_PRICE_LIMIT.min : GOLD_PRICE_LIMIT.max;
+                const forcedGoldPrice = matObj.currentPrice - priceChange
+                await updateNewPrice(material, forcedGoldPrice, staticGoldLimit, priceChange, matObj.limits.maxAmount);
+                break;
+            case "uranium":
+                const staticUraniumLimit = newPrice < matObj.limits.tempMinLimit ? URANIUM_PRICE_LIMIT.min : URANIUM_PRICE_LIMIT.max;
+                const forcedUraniumPrice = matObj.currentPrice - priceChange
+                await updateNewPrice(material, forcedUraniumPrice, staticUraniumLimit, priceChange, matObj.limits.maxAmount);
+                break;
+            case "ripCoin":
+                const staticRipCoinLimit = newPrice < matObj.limits.tempMinLimit ? RIPCOIN_PRICE_LIMIT.min : RIPCOIN_PRICE_LIMIT.max;
+                const forcedRipCoinPrice = matObj.currentPrice - priceChange
+                await updateNewPrice(material, forcedRipCoinPrice, staticRipCoinLimit, priceChange, matObj.limits.maxAmount);
+                break;
+            case "tibCoin":
+                const staticTibCoinLimit = newPrice < matObj.limits.tempMinLimit ? TIBCOIN_PRICE_LIMIT.min : TIBCOIN_PRICE_LIMIT.max;
+                const forcedTibPrice = matObj.currentPrice - priceChange
+                await updateNewPrice(material, forcedTibPrice, staticTibCoinLimit, priceChange, matObj.limits.maxAmount);
+                break;
+            case "diamond":
+                const staticDiamondCoinLimit = newPrice < matObj.limits.tempMinLimit ? DIAMOND_PRICE_LIMIT.min : DIAMOND_PRICE_LIMIT.max;
+                const forcedDiamondPrice = matObj.currentPrice - priceChange
+                await updateNewPrice(material, forcedDiamondPrice, staticDiamondCoinLimit, priceChange, matObj.limits.maxAmount);
+                break;
+            case "plutonium":
+                const staticPlutoniumCoinLimit = newPrice < matObj.limits.tempMinLimit ? PLUTONIUM_PRICE_LIMIT.min : PLUTONIUM_PRICE_LIMIT.max;
+                const forcedPlutoniumPrice = matObj.currentPrice - priceChange
+                await updateNewPrice(material, forcedPlutoniumPrice, staticPlutoniumCoinLimit, priceChange, matObj.limits.maxAmount);
+                break;
+            default:
+                console.log("No material match found")
+            
+        }
+    } else {
+        // console.log("no limit breached");
+        // when no limit reached reduce balancer stack by 1
+        const incrementBalancer = matObj.balancer < 0 ? 1 : matObj.balancer > 0 ? -1 : 0;
+        await updateNewPrice(material, newPrice, null, null, null, incrementBalancer);
+    }
+    
+}
+
+
+async function updateNewPrice(material, newPrice, staticLimit=null, priceChange=null, newLimitMaxRoll=null,  balancer=0) {
+    // balancer is little addition to randomized price increase or decrease.
+    // balancer is used for get higher chance to increase the price, when min limit reached, 
+    // and lower the increase chance, when max limit is reached. 
+    // if price reach min/max limit set random new min/max limit, and add 2 stack balancer value for next random increase roll.
+    if (staticLimit !== null && priceChange < 0) {
+        const data = {
+            currentPrice: newPrice,
+            history: FieldValue.arrayUnion({ timestamp: Date.now(), price: newPrice }),
+            'limits.tempMinLimit' : staticLimit + randomNumber(newLimitMaxRoll),
+            balancer: FieldValue.increment(2)
+        };
+
+        try {
+            await db.collection('prices').doc(material).update(data);   
+        } catch (err) {
+            console.error(err);
+        }
+    // if price reach max limit set random new max limit, and subtract 2 stack balancer value for next random increase roll.
+    } else if (staticLimit !== null && priceChange > 0) {
+        const data = {
+            currentPrice: newPrice,
+            history: FieldValue.arrayUnion({ timestamp: Date.now(), price: newPrice }),
+            'limits.tempMaxLimit' : staticLimit - randomNumber(newLimitMaxRoll),
+            balancer: FieldValue.increment(-2)
+        };
+
+        try {
+            await db.collection('prices').doc(material).update(data);      
+        } catch (err) {
+            console.error(err);
+        }
+    } else if (balancer !== 0) {
+        const data = {
+            currentPrice: newPrice,
+            history: FieldValue.arrayUnion({ timestamp: Date.now(), price: newPrice }),
+            balancer: FieldValue.increment(balancer)
+        };
+
+        try {
+            await db.collection('prices').doc(material).update(data);          
+        } catch (err) {
+            console.error(err);
+        }
+    } else {
+        const data = {
+            currentPrice: newPrice,
+            history: FieldValue.arrayUnion({ timestamp: Date.now(), price: newPrice }),
+        };
+
+        try {
+            await db.collection('prices').doc(material).update(data);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    try {
+        const allPrices = {}
+        allPrices[material] = newPrice;
+        await db.collection('prices').doc("currentPrices").update(allPrices);
+    } catch (err) {
+        console.error(err);
+    }
+
+}
+
+
+const clearOldHistoryData = async (material, Obj) => {
+    // remove price history data older than 1 day.
+    const now = new Date();
+    const tomorrowTimeStamp = now.setDate(now.getDate() - DAYS_AGO);
+    if (Obj?.history[0]?.timestamp && Obj?.history[0]?.timestamp < tomorrowTimeStamp) {
+        const filteredHistoryArray = Obj.history.filter(record => record.timestamp > tomorrowTimeStamp);
+        const data = {
+            history: filteredHistoryArray
+        }
+        await db.collection('prices').doc(material).update(data);
+    }
+}
+
+async function getPriceForMatch() {
+    const doc = await db.collection("prices").doc("currentPrices").get();
+    const pricesObj = doc.data();
+    return pricesObj;
+}
+
+
 async function setNewMaterialDoc() {
     // add new or set material/materials to the data base
     try {
@@ -105,152 +260,6 @@ async function setNewMaterialDoc() {
         console.error(err);
     }
     
-}
-
-async function updateNewPrice(material, newPrice, staticLimit=null, priceChange=null, newLimitMaxRoll=null,  balancer=0) {
-    // balancer is little addition to randomized price increase or decrease.
-    // balancer is used for get higher chance to increase the price, when min limit reached, 
-    // and lower the increase chance, when max limit is reached. 
-    // if price reach min/max limit set random new min/max limit, and add 2 stack balancer value for next random increase roll.
-    if (staticLimit !== null && priceChange < 0) {
-        const data = {
-            history: FieldValue.arrayUnion({ timestamp: Date.now(), price: newPrice }),
-            'limits.tempMinLimit' : staticLimit + randomNumber(newLimitMaxRoll),
-            balancer: FieldValue.increment(2)
-        };
-
-        try {
-            await db.collection('prices').doc(material).update(data);   
-        } catch (err) {
-            console.error(err);
-        }
-    // if price reach max limit set random new max limit, and subtract 2 stack balancer value for next random increase roll.
-    } else if (staticLimit !== null && priceChange > 0) {
-        const data = {
-            history: FieldValue.arrayUnion({ timestamp: Date.now(), price: newPrice }),
-            'limits.tempMaxLimit' : staticLimit - randomNumber(newLimitMaxRoll),
-            balancer: FieldValue.increment(-2)
-        };
-
-        try {
-            await db.collection('prices').doc(material).update(data);      
-        } catch (err) {
-            console.error(err);
-        }
-    } else if (balancer !== 0) {
-        const data = {
-            history: FieldValue.arrayUnion({ timestamp: Date.now(), price: newPrice }),
-            balancer: FieldValue.increment(balancer)
-        };
-
-        try {
-            await db.collection('prices').doc(material).update(data);          
-        } catch (err) {
-            console.error(err);
-        }
-    } else {
-        const data = {
-            history: FieldValue.arrayUnion({ timestamp: Date.now(), price: newPrice }),
-        };
-
-        try {
-            await db.collection('prices').doc(material).update(data);
-        } catch (err) {
-            console.error(err);
-        }
-    }
-    try {
-        const allPrices = {}
-        allPrices[material] = newPrice;
-        await db.collection('prices').doc("currentPrices").update(allPrices);
-    } catch (err) {
-        console.error(err);
-    }
-
-}
-
-const randomNewPrice = async (material) => {
-    // check if price reach any tempLimit true => set new temp limit
-    const matObj = (await db.collection('prices').doc(material).get()).data();
-    clearOldHistoryData(material, matObj);
-    const increase = randomNumber(matObj.limits.maxAmount);
-    if (matObj.currentPrice < matObj.limits.tempMinLimit) {
-        const newPrice = matObj.currentPrice + Math.floor(increase / 2);
-        await updateNewPrice(material, newPrice);
-        return
-    } else if (matObj.currentPrice > matObj.limits.tempMaxLimit) {
-        const newPrice = matObj.currentPrice - Math.floor(increase / 2);
-        await updateNewPrice(material, newPrice);
-        return
-    }
-    const priceChange = increase - Math.floor(matObj.limits.maxAmount / 2);
-    const newPrice = matObj.balancer !== 0 
-        ? matObj.currentPrice + priceChange + matObj.balancer * matObj.limits.maxAmount / 10 
-        : matObj.currentPrice + priceChange;
-
-    if (newPrice < matObj.limits.tempMinLimit || newPrice > matObj.limits.tempMaxLimit) {
-        switch (material) {
-            case "gold":
-                const staticGoldLimit = newPrice < matObj.limits.tempMinLimit ? GOLD_PRICE_LIMIT.min : GOLD_PRICE_LIMIT.max;
-                const forcedGoldPrice = matObj.currentPrice - priceChange
-                await updateNewPrice(material, forcedGoldPrice, staticGoldLimit, priceChange, matObj.limits.maxAmount);
-                break;
-            case "uranium":
-                const staticUraniumLimit = newPrice < matObj.limits.tempMinLimit ? URANIUM_PRICE_LIMIT.min : URANIUM_PRICE_LIMIT.max;
-                const forcedUraniumPrice = matObj.currentPrice - priceChange
-                await updateNewPrice(material, forcedUraniumPrice, staticUraniumLimit, priceChange, matObj.limits.maxAmount);
-                break;
-            case "ripCoin":
-                const staticRipCoinLimit = newPrice < matObj.limits.tempMinLimit ? RIPCOIN_PRICE_LIMIT.min : RIPCOIN_PRICE_LIMIT.max;
-                const forcedRipCoinPrice = matObj.currentPrice - priceChange
-                await updateNewPrice(material, forcedRipCoinPrice, staticRipCoinLimit, priceChange, matObj.limits.maxAmount);
-                break;
-            case "tibCoin":
-                const staticTibCoinLimit = newPrice < matObj.limits.tempMinLimit ? TIBCOIN_PRICE_LIMIT.min : TIBCOIN_PRICE_LIMIT.max;
-                const forcedTibPrice = matObj.currentPrice - priceChange
-                await updateNewPrice(material, forcedTibPrice, staticTibCoinLimit, priceChange, matObj.limits.maxAmount);
-                break;
-            case "diamond":
-                const staticDiamondCoinLimit = newPrice < matObj.limits.tempMinLimit ? DIAMOND_PRICE_LIMIT.min : DIAMOND_PRICE_LIMIT.max;
-                const forcedDiamondPrice = matObj.currentPrice - priceChange
-                await updateNewPrice(material, forcedDiamondPrice, staticDiamondCoinLimit, priceChange, matObj.limits.maxAmount);
-                break;
-            case "plutonium":
-                const staticPlutoniumCoinLimit = newPrice < matObj.limits.tempMinLimit ? PLUTONIUM_PRICE_LIMIT.min : PLUTONIUM_PRICE_LIMIT.max;
-                const forcedPlutoniumPrice = matObj.currentPrice - priceChange
-                await updateNewPrice(material, forcedPlutoniumPrice, staticPlutoniumCoinLimit, priceChange, matObj.limits.maxAmount);
-                break;
-            default:
-                console.log("No material match found")
-            
-        }
-    } else {
-        // console.log("no limit breached");
-        // when no limit reached reduce balancer stack by 1
-        const incrementBalancer = matObj.balancer < 0 ? 1 : matObj.balancer > 0 ? -1 : 0;
-        await updateNewPrice(material, newPrice, null, null, null, incrementBalancer);
-    }
-    
-}
-
-
-const clearOldHistoryData = async (material, Obj) => {
-    // remove price history data older than 1 day.
-    const now = new Date();
-    const tomorrowTimeStamp = now.setDate(now.getDate() - DAYS_AGO);
-    if (Obj?.history[0]?.timestamp && Obj?.history[0]?.timestamp < tomorrowTimeStamp) {
-        const filteredHistoryArray = Obj.history.filter(record => record.timestamp > tomorrowTimeStamp);
-        const data = {
-            history: filteredHistoryArray
-        }
-        await db.collection('prices').doc(material).update(data);
-    }
-}
-
-async function getPriceForMatch() {
-    const doc = await db.collection("prices").doc("currentPrices").get();
-    const pricesObj = doc.data();
-    return pricesObj;
 }
 
 
