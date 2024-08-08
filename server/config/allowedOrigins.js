41b84154b5c3c67ca8cc168055404b8bff2b4a4e
Sacredice <122034500+Sacredice@users.noreply.github.com>
2024-08-08 20:54:18 +0300

    cors whitelist updated

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'https://contrivedtradingapp.onrender.com',
+    'https://contrivedtradingapp.onrender.com/index.html',
 ]
 
 module.exports = allowedOrigins
