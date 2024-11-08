139a85a0e61ba2d8a4af44683742103ed842d8ea
Sacredice <122034500+Sacredice@users.noreply.github.com>
2024-11-08 15:17:11 +0300

    Footer content and style

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,18 @@
 import { useColorMode } from '../context/ColorModeContext'
 
 function Footer() {
-    const { checked } = useColorMode();
+  const { checked } = useColorMode();
+
 
   return (
-    <div style={{ color: checked ? "whitesmoke" : "black", background: checked ? "#272727" : "#2196F3", marginTop: "auto", width: "100%", height: "40px" }}>
-      <p>Footer</p>
-    </div>
+    <footer style={{ display: "flex", justifyContent: "space-between", color: checked ? "whitesmoke" : "black", background: checked ? "#272727" : "#2196F3", marginTop: "auto", height: "40px" }}>
+      <div style={{ display: "flex", alignItems: "center", marginLeft: "2rem" }}>
+        <p>Contrived Trading App &copy; {(new Date()).getFullYear()}</p>
+      </div>
+      <div style={{ display: "flex", alignItems: "center", marginRight: "2rem" }}>
+        <a href="https://github.com/Sacredice/contrivedTradeSite" target='_blank'><img style={{ width: "1.5rem" }} src="/github-mark-white.png" alt="" /></a>
+      </div>
+    </footer>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,20 +11,20 @@ function Home() {
 
 
   return (
-    <div style={{ backgroundColor: "rgba(255, 255, 255, 0)", display: "flex", justifyContent: "center", alignItems: "start",  marginTop: "16px" }}>
-      <div style={{ display: "flex",flexDirection: "column", width: "75%", justifyContent: "center", alignItems: "center", margin: "0", color: checked ? "whitesmoke" : "black" }}>
-        <h1 className="homeHeader" style={{ marginBottom: "16px"}}>Welcome to my MERN Stack Project</h1>
+    <div style={{ backgroundColor: "rgba(255, 255, 255, 0)", display: "flex", justifyContent: "center", alignItems: "start", marginTop: "16px" }}>
+      <div style={{ display: "flex", flexDirection: "column", width: "75%", justifyContent: "center", alignItems: "center", marginBottom: "2rem", color: checked ? "whitesmoke" : "black" }}>
+        <h1 className="homeHeader" style={{ marginBottom: "16px" }}>Welcome to my MERN Stack Project</h1>
         <p className='home'>You need to login to start buying and selling the 6 type of investments. Prices of investments change by a random amount within a certain limit after a randomly determined period of time between 6 and 2.5 minutes. After signing up, every account will receive 250,000 credits, and there is no transaction fee.
-        <br />
-        <br />
-        For security concerns, you can use a fake email address and a simple password (not the one you use for other accounts). There is no email confirmation process, and don't worry; only your password hash is stored in the database.
-        Or you can log in to the test account for testing using the <b>username: test</b>, <b>password: pass</b>.  
-        <br />
-        <br />
-        Login could take up to 50 seconds to spin up the server if it is spun down. It's free tier render service.
-        <br />
-        <br />
-        Firebase is used to store current prices and price history for real-time data synchronization, so prices update automatically. 
+          <br />
+          <br />
+          For security concerns, you can use a fake email address and a simple password (not the one you use for other accounts). There is no email confirmation process, and don't worry; only your password hash is stored in the database.
+          Or you can log in to the test account for testing using the <b>username: test</b>, <b>password: pass</b>.
+          <br />
+          <br />
+          Login could take up to 50 seconds to spin up the server if it is spun down. It's free tier render service.
+          <br />
+          <br />
+          Firebase is used to store current prices and price history for real-time data synchronization, so prices update automatically.
         </p>
       </div>
     </div>
